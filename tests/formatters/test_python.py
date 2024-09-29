66263fd019f58e3ec02d58de3b42c7ef5293acb4
Thomas Weise <tweise@hfuu.edu.cn>
2024-09-29 18:44:54 +0800

    now *really* changed default number of permitted empty lines from 1 to 2 in Python formatter (2)

--- tests/formatters/test_python.py
+++ tests/formatters/test_python.py
@@ -3,13 +3,13 @@
 from typing import Final
 
 from pycommons.io.path import Path, file_path
-from pycommons.processes.python import python_command
+from pycommons.processes.python import PYTHON_ENV, python_command
 from pycommons.processes.shell import STREAM_CAPTURE, Command
 
 import latexgit.formatters.python as fp
 
 
-def test_python() -> None:
+def test_python_1() -> None:
     """Test the python formatter."""
     sf: Final[Path] = file_path(__file__)
     source: Final[str] = sf.read_all_str()
@@ -18,7 +18,21 @@ def test_python() -> None:
     call.extend(["--lines", "1-6", "--args", "format"])
     formatted = Command(
         call, stdin=source, stdout=STREAM_CAPTURE,
-        working_dir=wd).execute()[0]
+        working_dir=wd, env=PYTHON_ENV).execute()[0]
     assert isinstance(formatted, str)
     lines: list[str] = formatted.split("\n")
     assert len(lines) == 7
+
+
+def test_python_2() -> None:
+    """Test the python formatter."""
+    sf: Final[Path] = file_path(__file__)
+    source: Final[str] = str.rstrip(sf.read_all_str()[12:38])
+    wd: Path = sf.up(3)
+    call: list[str] = list(python_command(fp.__file__))
+    call.extend(["--args", "format"])
+    formatted = Command(
+        call, stdin=source, stdout=STREAM_CAPTURE,
+        working_dir=wd, env=PYTHON_ENV).execute()[0]
+    assert isinstance(formatted, str)
+    assert str.rstrip(formatted) == source
